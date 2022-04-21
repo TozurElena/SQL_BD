6a5aa99a379a1f425678de5b5c70636919a0bef8
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-04-21 15:10:53 +0200

    add formilaire

--- php-pdo/index.php
+++ php-pdo/index.php
@@ -13,7 +13,13 @@
     {
 	  // On se connecte à MySQL
 	    $bdd = new PDO('mysql:host=localhost;dbname=weatherapp;charset=utf8', 'root', '');
-      $result = $bdd->query('SELECT * FROM météo');
+    }
+    catch(Exception $e)
+    {
+	    // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+    }
+    $result = $bdd->query('SELECT * FROM meteo');
       // $donnees = $result->fetch();
       // print_r($donnees);
      
@@ -39,15 +45,39 @@
       }
       echo '</tbody>';
       echo '</table>';
-      $result->closeCursor();
-
-  
-    }
-    catch(Exception $e)
-    {
-	    // En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
-    }
+    
+    $result->closeCursor();
+       
   ?>
+
+<h2>Ajouter d'autres villes</h2>
+<form method="POST" action="">
+	<label for="ville">Ville: </label>
+	<input type="text" name="ville">
+	<label for="max"><br>Temperature max:</label>
+	<input type="number" name="max" >
+  <label for="min"><br>Temperature min:</label>
+	<input type="number" name="min" >
+	<input type="submit" name="submit" value="Add">
+</form>
+
+<?php
+  
+	if (isset($_POST['ville'], $_POST['max'], $_POST['min'])) {
+		 $sqlQuery = 'INSERT INTO meteo(ville, haut, bas) VALUES (:ville, :haut, :bas)';
+    //preparation
+    $insertVille = $bdd->prepare($sqlQuery);
+    //Execution. La ville en base de données
+    $insertVille->execute([
+      'ville' => $_POST['ville'],
+      'haut' => $_POST['max'],
+      'bas' => $_POST['min'],
+    ]);
+    $insertVille->closeCursor();		
+	} else echo('Erreur  ');
+
+  // header("Location:/sql/php-pdo/index.php");
+  
+?> 
 </body>
 </html>
