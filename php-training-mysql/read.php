4182df5c10f51d712843540cd6083649fc7a0ef8
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-04-25 16:26:36 +0200

    Afficher la liste des randonnées

--- php-training-mysql/read.php
+++ php-training-mysql/read.php
@@ -1,3 +1,13 @@
+<!-- CONNEXION À LA BASE DE DONNÉES -->
+<?php
+try {
+  // Connexion à MySQL
+  $bd = new PDO('mysql:host=localhost;dbname=becode;charset=utf8', 'root', '');
+} catch (Exception $e) {
+  die('Erreur : ' . $e->getMessage());
+}
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,8 +17,34 @@
   </head>
   <body>
     <h1>Liste des randonnées</h1>
+    <?php
+   //réalisation requete
+    $result = $bd->query('SELECT * FROM hiking');
+  ?>
     <table>
       <!-- Afficher la liste des randonnées -->
+      <thead>
+          <tr>
+            <th> Name</th>
+            <th> Dificulty</th>
+            <th> Distance</th>
+            <th> Duration</th>
+            <th> Dénivelé</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php while ($donnees = $result->fetch()) : ?>
+        <tr>
+          <td><?php echo ($donnees['name']);?></td> 
+          <td><?php echo ($donnees['difficulty']); ?></td>
+          <td><?php echo ($donnees['distance']); ?></td>
+          <td><?php echo ($donnees['duration']); ?></td>
+          <td><?php echo ($donnees['height_difference']); ?></td>
+        </tr>
+        <?php endwhile;
+        $result->closeCursor();
+        ?>
+        </tbody>
     </table>
   </body>
 </html>
